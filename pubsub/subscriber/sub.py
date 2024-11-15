af649d1bf2b09c1ad6efa836da370394e5930faa
Davidemanoel7 <david.emanoel.706@ufrn.edu.br>
2024-11-15 19:50:27 -0300

    Message queue and some fixes...

--- pubsub/subscriber/sub.py
+++ pubsub/subscriber/sub.py
@@ -1,11 +1,16 @@
 import random
-import signal
+# import signal
 from paho.mqtt import client as mqtt_client
 
-host = '127.0.0.1'
-port = 1883
-topic = "calc/op"
+host = '127.0.0.53'
+port = 8080
+topic = "calc/op/"
 client_id = f'python-mqtt-{random.randint(0, 100)}'
+
+message_queue = {
+    "last_message": "",
+    "index": -1
+}
 
 # signal = signal.signal( signal.SIGINT, quit )
 
@@ -17,9 +22,7 @@ def connect_mqtt() -> mqtt_client:
         else:
             print("Failed to connect, return code %d\n", rc)
 
-    client = mqtt_client.Client(
-        callback_api_version= mqtt_client.CallbackAPIVersion.VERSION2,
-        client_id=client_id )
+    client = mqtt_client.Client( callback_api_version= mqtt_client.CallbackAPIVersion.VERSION2 )
     
     client.on_connect = on_connect
     client.connect( host=host, port=port, keepalive=60 )
@@ -28,30 +31,42 @@ def connect_mqtt() -> mqtt_client:
 
 
 def subscribe( client: mqtt_client ):
-    client.subscribe(topic)
+    client.subscribe( topic )
     client.on_message = on_message
 
-def on_message( client, userdata, msg ):
-        # msg = msg.payload.decode()
-        print(f"Easy: {msg.payload}")
+def on_message( client: mqtt_client, userdata, msg ):
+        decoded_msg = msg.payload.decode()
+        # print(f"Receiv message: {msg}")
+        
+        ind = int(message_queue.get("index") + 1)
 
-        # op = msg[0]
+        message_queue.update( {"last_message": f"{ decoded_msg }"} )
+        message_queue.update( {"index": ind } )
 
-        # match op:
-        #     case 'ADD':
-        #         print(add( 1, 1 ))
-        #         return
-        #     case 'SUB':
-        #         print(sub( 1, 1 ))
-        #         return
-        #     case 'MUL':
-        #         print(mul( 1, 1))
-        #         return
-        #     case 'DIV':
-        #         print(div( 1, 1 ))
-        #         return
-        #     case __:
-        #         return
+        # print( message_queue )
+
+        split_msg = decoded_msg.split(" ")
+
+        op = split_msg[0]
+        a = split_msg[1]
+        b = split_msg[2]
+
+        match op:
+            case 'ADD':
+                print(add( a, b ))
+                return
+            case 'SUB':
+                print(sub( a, b ))
+                return
+            case 'MUL':
+                print(mul( a, b ))
+                return
+            case 'DIV':
+                print(div( a, b ))
+                return
+            case __:
+                return
+
 
 
 def on_disconnect(client, userdata, flags, rc, properties):
@@ -60,6 +75,7 @@ def on_disconnect(client, userdata, flags, rc, properties):
 
 def run():
     client = connect_mqtt()
+
     subscribe(client)
 
     client.on_disconnect = on_disconnect
